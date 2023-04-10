7a4d4871b630a7a6e51a6405996d4d6261309d29
inderjit-fullstack-dev <inderjit.fullstack.dev@gmail.com>
2023-04-10 12:19:49 +0530

    initial Commit

---
+++ Calculator.cs
@@ -0,0 +1,24 @@
+public class Calculator : ICalculator 
+{
+    public Calculator() {}
+
+    public int Addition(int a, int b)
+    {
+        // No implementation
+    }
+
+    public int Subtraction(int a, int b)
+    {
+        // No implementation
+    }
+
+    public int Multiplication(int a, int b)
+    {
+        // No implementation
+    }
+
+    public int Division(int a, int b)
+    {
+        // No implementation
+    }
+}
---
+++ ICalculator.cs
@@ -0,0 +1,7 @@
+public interface ICalculator 
+{
+    public int Addition(int a, int b);
+    public int Subtraction(int a, int b);
+    public int Multiplication(int a, int b);
+    public int Division(int a, int b);
+}
